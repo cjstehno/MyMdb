89a93a9c2361e7c88c3a055413306767f120ea2b
cjstehno <chris@stehno.com>
2010-01-16 16:36:29 -0600

    actor unit testing

--- test/unit/com/stehno/mymdb/domain/ActorTests.groovy
+++ test/unit/com/stehno/mymdb/domain/ActorTests.groovy
@@ -8,10 +8,64 @@ class ActorTests extends GrailsUnitTestCase {
 		assertTrue actor(firstName:'Larry',middleName:'Q',lastName:'Stooge').validate()
     }
 
-	void testValidation_firstName_empty() {
+	void testValidation_firstName_missing() {
 		def actor = actor(middleName:'Q',lastName:'Stooge')
-		assertFalse actor.validate()
-		assertLength 1, actor.errors
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_firstName_empty() {
+		def actor = actor(firstName:'',middleName:'Q',lastName:'Stooge')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_firstName_too_short() {
+		def actor = actor(firstName:'x',middleName:'Q',lastName:'Stooge')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_firstName_too_long() {
+		def actor = actor(firstName:('x'*26),middleName:'Q',lastName:'Stooge')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_middleName_missing() {
+		def actor = actor(firstName:'Larry',lastName:'Stooge')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_middleName_empty() {
+		def actor = actor(firstName:'Larry',middleName:'',lastName:'Stooge')
+		assertTrue actor.validate()
+    }
+
+	void testValidation_middleName_too_long() {
+		def actor = actor(firstName:'Larry',middleName:('x'*26),lastName:'Stooge')
+		assertInvalidWithOneError actor
+    }
+
+	private void assertInvalidWithOneError( dom ){
+		assertFalse dom.validate()
+		assertLength 1, dom.errors
+	}
+
+	void testValidation_lastName_missing() {
+		def actor = actor(firstName:'Larry',middleName:'Q')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_lastName_empty() {
+		def actor = actor(firstName:'Larry',middleName:'Q',lastName:'')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_lastName_too_short() {
+		def actor = actor(firstName:'Larry',middleName:'Q',lastName:'x')
+		assertInvalidWithOneError actor
+    }
+
+	void testValidation_lastName_too_long() {
+		def actor = actor(firstName:'Larry',middleName:'Q',lastName:('x'*26))
+		assertInvalidWithOneError actor
     }
 
 	private Actor actor(params){
